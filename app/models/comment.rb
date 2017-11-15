eecc36650d145afd0207ecc493528b81b8310dfb
Christine Tran <christinetran825@gmail.com>
2017-11-15 06:21:54 +0000

    automatically backed up by learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,9 +4,9 @@ class Comment < ActiveRecord::Base
 
   accepts_nested_attributes_for :user
 
-  def user_attributes=(user_attribute)
-    if user_attribute[:username] != ""
-      self.create_user(user_attribute)
+  def user_attributes=(user_attributes)
+    if user_attributes[:username] != ""
+      self.create_user(user_attributes)
     end
   end
 
