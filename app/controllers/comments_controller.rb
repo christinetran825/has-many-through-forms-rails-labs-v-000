a29eea520571bb4a2e00a5305edcdd4c93c54082
Christine Tran <christinetran825@gmail.com>
2017-11-15 05:57:48 +0000

    automatically backed up by learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,17 +5,18 @@ class CommentsController < ApplicationController
     redirect_to comment.post
   end
 
-  private
-
-  def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
-  end
-
   def user_attributes=(user_attributes)
     user_attributes.values.each do |user_attribute|
       user = User.find_or_create_by(user_attribute)
       self.users << user
     end
+  end
+
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
   end
 
 end
